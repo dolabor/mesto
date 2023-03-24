82f6cf9ce70c5d01d90eaade035115c123cb0489
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-03-24 23:21:02 +0300

    fix: исправить config.js popup__input-error

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 const editFormConfig = {
   formSelector: '#edit-profile-form form',
   inputSelector: '#edit-profile-form .popup__input',
-  errorClassTemplate: '.popup__input_error_type_',
+  errorClassTemplate: '.popup__input-error_type_',
   activeErrorClass: 'popup__input-error_active',
   submitButtonSelector: '#edit-profile-form .popup__submit-button',
   invalidSubmitButtonClass: 'popup__submit-button_inactive'
@@ -10,7 +10,7 @@ const editFormConfig = {
 const addFormConfig = {
   formSelector: '#add-place-form form',
   inputSelector: '#add-place-form .popup__input',
-  errorClassTemplate: '.popup__input_error_type_',
+  errorClassTemplate: '.popup__input-error_type_',
   activeErrorClass: 'popup__input-error_active',
   submitButtonSelector: '#add-place-form .popup__submit-button',
   invalidSubmitButtonClass: 'popup__submit-button_inactive'
